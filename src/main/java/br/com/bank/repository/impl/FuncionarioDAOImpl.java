884ee2dc82ea43fb4d303a6936b90d1c241aa053
gabrieloftas <gabrieloftas@outlook.com>
2025-08-14 09:43:14 -0300

    fix: Correção dos metodos update e delete

--- src/main/java/br/com/bank/repository/impl/FuncionarioDAOImpl.java
+++ src/main/java/br/com/bank/repository/impl/FuncionarioDAOImpl.java
@@ -1,5 +1,6 @@
 package br.com.bank.repository.impl;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -10,12 +11,12 @@ import br.com.bank.repository.FuncionarioDAO;
 
 public class FuncionarioDAOImpl implements FuncionarioDAO {
 
-    private static final Map<String,Funcionario> DATABASE = new HashMap<>();
+    private static final Map<String, Funcionario> DATABASE = new HashMap<>();
 
 
     @Override
     public void create(Funcionario typeClass) {
-        DATABASE.put(typeClass.getCpf(),typeClass);
+        DATABASE.put(typeClass.getCpf(), typeClass);
     }
 
     @Override
@@ -30,11 +31,15 @@ public class FuncionarioDAOImpl implements FuncionarioDAO {
 
     @Override
     public void update(String key, Funcionario typeClass) {
-        DATABASE.put(key,typeClass);
+        if (DATABASE.containsKey(key)) {
+            DATABASE.put(key, typeClass);
+        } else throw new IllegalArgumentException();
     }
 
     @Override
     public void delete(String key) {
-        DATABASE.remove(key);
+        if (DATABASE.remove(key) == null) {
+            throw new IllegalArgumentException();
+        }
     }
 }
