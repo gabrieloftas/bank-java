a1498dd07cca52ad334ecff9c982f2d9d3044630
gabrieloftas <gabrieloftas@outlook.com>
2025-08-14 09:28:33 -0300

    fix: atualização do ClienteDAOImpl

--- src/main/java/br/com/bank/repository/impl/ClienteDAOImpl.java
+++ src/main/java/br/com/bank/repository/impl/ClienteDAOImpl.java
@@ -8,14 +8,14 @@ import java.util.Arrays;
 import java.util.List;
 
 public class ClienteDAOImpl implements ClienteDAO {
-    private static final Cliente[] CLIENTES = new Cliente[30];
+    private static final Cliente[] DATABASE = new Cliente[30];
 
 
     @Override
     public void create(Cliente typeClass) {
-        for (int i = 0; i < CLIENTES.length; i++) {
-            if (CLIENTES[i] == null) {
-                CLIENTES[i] = typeClass;
+        for (int i = 0; i < DATABASE.length; i++) {
+            if (DATABASE[i] == null) {
+                DATABASE[i] = typeClass;
                 return;
             }
         }
@@ -23,31 +23,50 @@ public class ClienteDAOImpl implements ClienteDAO {
 
     @Override
     public Cliente get(String key) {
-        //Duvida
-        for (int i = 0; i < CLIENTES.length; i++) {
-            if (CLIENTES[i].equals(Cliente));
+        try{
+            return DATABASE[getIndex(key)];
+        }
+        catch (IndexOutOfBoundsException e) {
+            return null;
         }
     }
 
     @Override
     public List<Cliente> getAll() {
-        return new ArrayList<Cliente>(Arrays.stream(CLIENTES).toList());
+        return new ArrayList<Cliente>(Arrays.stream(DATABASE).toList());
     }
 
     @Override
     public void update(String key, Cliente typeClass) {
-        for (int i = 0;i < CLIENTES.length; i++){
-            if (CLIENTES[i].equals(typeClass)){
-                CLIENTES[i]=typeClass;
+        for (int i = 0; i < DATABASE.length; i++) {
+            if (DATABASE[i].equals(typeClass)) {
+                DATABASE[i] = typeClass;
             }
         }
     }
 
     @Override
     public void delete(String key) {
-        //Duvida
+        try{
+            DATABASE[getIndex(key)]=null;
+        } catch (Exception e) {
+            throw new RuntimeException("Não foi possível excluir o cliente");
+        }
+
+    }
+
+    private int getIndex(String key) {
+        for (int i = 0; i < DATABASE.length; i++) {
+            if (DATABASE[i].getCpf().equals(key)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
+
+
+
 
 
 
